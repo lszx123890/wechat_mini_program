89f6ecc969084427017b926540f57d615041ff33
lszx123890 <lszx172@163.com>
2025-12-18 23:44:59 +0800

    Update banners.js

--- utils/data/banners.js
+++ utils/data/banners.js
@@ -1,22 +1,22 @@
 {
   "bannerList": [
     {
-      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner1.png"
+      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner1.png"
     },
     {
-      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner2.png"
+      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner2.png"
     },
     {
-      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner3.png"
+      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner3.png"
     },
     {
-      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner4.png"
+      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner4.png"
     },
     {
-      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner5.png"
+      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner5.png"
     },
     {
-      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner6.png"
+      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner6.png"
     }
   ]
 }
