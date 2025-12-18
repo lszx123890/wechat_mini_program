5a98d3a9f94c144c9d9197d9bce11823f1b38b4e
lszx123890 <lszx172@163.com>
2025-12-18 11:58:56 +0800

    Update strategyList.js

--- utils/data/strategyList.js
+++ utils/data/strategyList.js
@@ -1,28 +1,25 @@
-// 备考攻略数据
-const strategyList = [
-  {
-    id: 1,
-    cover: '/page/component/resources/image/finished.png',
-    title: '科目一高频易错题汇总',
-    desc: '整理了科目一考试中最容易出错的50道题，看完通过率提升90%',
-    readNum: 12580
-  },
-  {
-    id: 2,
-    cover: '/page/component/resources/image/finished.png',
-    title: '科目四答题技巧口诀',
-    desc: '记住这些口诀，科目四轻松拿满分，不用死记硬背',
-    readNum: 8960
-  },
-  {
-    id: 3,
-    cover: '/page/component/resources/image/finished.png',
-    title: '2025驾考新规解读',
-    desc: '最新驾考规则变化，新增题型及扣分标准',
-    readNum: 15600
-  }
-];
-
-module.exports = {
-  strategyList: strategyList
-};
+{
+  "strategyList": [
+    {
+      "id": 1,
+      "cover": "/page/component/resources/image/finished.png",
+      "title": "科目一高频易错题汇总",
+      "desc": "整理了科目一考试中最容易出错的50道题，看完通过率提升90%",
+      "readNum": 12580
+    },
+    {
+      "id": 2,
+      "cover": "/page/component/resources/image/finished.png",
+      "title": "科目四答题技巧口诀",
+      "desc": "记住这些口诀，科目四轻松拿满分，不用死记硬背",
+      "readNum": 8960
+    },
+    {
+      "id": 3,
+      "cover": "/page/component/resources/image/finished.png",
+      "title": "2025驾考新规解读",
+      "desc": "最新驾考规则变化，新增题型及扣分标准",
+      "readNum": 15600
+    }
+  ]
+}
