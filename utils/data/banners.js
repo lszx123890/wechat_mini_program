cf6765093c164963b1ae8ba217d7624c497508ca
lszx123890 <lszx172@163.com>
2025-12-18 22:18:27 +0800

    Update banners.js

--- utils/data/banners.js
+++ utils/data/banners.js
@@ -1,4 +1,4 @@
-{
+/*{
   "bannerList": [{
     {
       imgUrl: 'https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner1.png'
@@ -19,4 +19,23 @@
       imgUrl: 'https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner6.png'
     }}
     ]
+}*/
+{
+  "bannerList": [
+    {
+      "id": "1",
+      "name": "柳州汉华科目三",
+      "address": "柳州市柳南区柳工大道168号",
+      "contact": "0772-1234567",
+      "routeIds": [1, 2, 3]
+    },
+    {
+      "id": "2",
+      "name": "测试线路",
+      "address": "南宁市江南区那洪大道78号",
+      "contact": "0771-7654321",
+      "routeIds": [4, 5]
+    }
+    // 其他考场...
+  ]
 }
