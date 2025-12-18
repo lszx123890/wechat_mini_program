bb24ff278b5180826a2dcaf44f15176e1af8fbf8
lszx123890 <lszx172@163.com>
2025-12-18 22:22:07 +0800

    Update banners.js

--- utils/data/banners.js
+++ utils/data/banners.js
@@ -1,41 +1,22 @@
-/*{
-  "bannerList": [{
-    {
-      imgUrl: 'https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner1.png'
-    },
-    {
-      imgUrl: 'https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner2.png'
-    },
-    {
-      imgUrl: 'https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner3.png'
-    },
-    {
-      imgUrl: 'https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner4.png'
-    },
-    {
-      imgUrl: 'https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner5.png'
-    },
-    {
-      imgUrl: 'https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner6.png'
-    }}
-    ]
-}*/
 {
   "bannerList": [
     {
-      "id": "1",
-      "name": "柳州汉华科目三",
-      "address": "柳州市柳南区柳工大道168号",
-      "contact": "0772-1234567",
-      "routeIds": [1, 2, 3]
+      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner1.png"
     },
     {
-      "id": "2",
-      "name": "测试线路",
-      "address": "南宁市江南区那洪大道78号",
-      "contact": "0771-7654321",
-      "routeIds": [4, 5]
+      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner2.png"
+    },
+    {
+      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner3.png"
+    },
+    {
+      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner4.png"
+    },
+      {
+      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner5.png"
+    },
+        {
+      "imgUrl": "https://fastly.jsdelivr.net/gh/lszx123890/wechat_mini_program/image/banner/banner6.png"
     }
-    // 其他考场...
   ]
 }
