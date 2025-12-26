c56e21b38b4899d5a0328536a607aff7fd1cfb97
lszx123890 <lszx172@163.com>
2025-12-26 22:03:59 +0800

    Update banners.js

--- utils/data/banners.js
+++ utils/data/banners.js
@@ -1,22 +1,22 @@
 {
   "bannerList": [
     {
-      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program@v0.0.2/image/banner/banner1.png"
+      "imgUrl": "/image/banner/banner1.png"
     },
     {
-      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program@v0.0.2/image/banner/banner2.png"
+      "imgUrl": "/image/banner/banner2.png"
     },
     {
-      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program@v0.0.2/image/banner/banner3.png"
+      "imgUrl": "/image/banner/banner3.png"
     },
     {
-      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program@v0.0.2/image/banner/banner4.png"
+      "imgUrl": "/image/banner/banner4.png"
     },
     {
-      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program@v0.0.2/image/banner/banner5.png"
+      "imgUrl": "/image/banner/banner5.png"
     },
     {
-      "imgUrl": "https://gcore.jsdelivr.net/gh/lszx123890/wechat_mini_program@v0.0.2/image/banner/banner6.png"
+      "imgUrl": "/image/banner/banner6.png"
     }
   ]
 }
