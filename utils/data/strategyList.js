837dc96db4d10d0cec645afaed6c5031a2740f3d
lszx123890 <lszx172@163.com>
2026-01-01 15:55:16 +0800

    Update strategyList.js

--- utils/data/strategyList.js
+++ utils/data/strategyList.js
@@ -2,6 +2,7 @@
   "strategyList": [
     {
       "id": 1,
+      "strategy":"科目一",
       "cover": "/page/component/resources/image/finished.png",
       "title": "科目一高频易错题汇总",
       "desc": "整理了科目一考试中最容易出错的50道题，看完通过率提升90%",
@@ -9,6 +10,7 @@
     },
     {
       "id": 2,
+      "strategy":"科目四",
       "cover": "/page/component/resources/image/finished.png",
       "title": "科目四答题技巧口诀",
       "desc": "记住这些口诀，科目四轻松拿满分，不用死记硬背",
@@ -16,6 +18,7 @@
     },
     {
       "id": 3,
+      "strategy":"科目二",
       "cover": "/page/component/resources/image/finished.png",
       "title": "2025驾考新规解读",
       "desc": "最新驾考规则变化，新增题型及扣分标准",
@@ -23,6 +26,7 @@
     },
     {
       "id": 4,
+      "strategy":"科目三",
       "cover": "/page/component/resources/image/finished.png",
       "title": "熟悉科目三考场路线就用嘟嘟驾学",
       "desc": "在考场模拟，一次要花几百，还记不清路线。可以先用嘟嘟驾学熟悉线路",
