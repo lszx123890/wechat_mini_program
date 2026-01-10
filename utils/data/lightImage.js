3bee9ecbed3053505f94bd2364e21c4680fb77a0
lszx123890 <lszx172@163.com>
2026-01-10 17:10:10 +0800

    Add files via upload

---
+++ utils/data/lightImage.js
@@ -0,0 +1,36 @@
+{
+  "lightImages": [
+    {
+		"id":"bg",
+		"imgUrl": "/image/light/bg.png"
+    },
+    {
+		"id":"alarm",
+      "imgUrl": "/image/light/alarm.png"
+    },
+    {
+		"id":"highLight",
+      "imgUrl": "/image/light/highLight.png"
+    },
+    {
+		"id":"turnLight",
+      "imgUrl": "/image/light/turnLight.png"
+    },
+    {
+		"id":"switch",
+      "imgUrl": "/image/light/switch.png"
+    },
+    {
+		"id":"highLamp",
+      "imgUrl": "/image/light/highLamp.png"
+    },
+    {
+		"id":"lowLamp",
+      "imgUrl": "/image/light/lowLamp.png"
+    },
+    {
+		"id":"positionLamp",
+      "imgUrl": "/image/light/positionLamp.png"
+    }
+  ]
+}
