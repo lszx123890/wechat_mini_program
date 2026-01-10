d0241b3211d72a9ab3f9c3d2fa9f31e745f9e4d3
lszx123890 <lszx172@163.com>
2026-01-10 17:21:25 +0800

    Update lightImages.js

--- utils/data/lightImages.js
+++ utils/data/lightImages.js
@@ -1,36 +1,14 @@
 {
   "lightImages": [
     {
-		"id":"bg",
-		"imgUrl": "/image/light/bg.png"
-    },
-    {
-		"id":"alarm",
-      "imgUrl": "/image/light/alarm.png"
-    },
-    {
-		"id":"highLight",
-      "imgUrl": "/image/light/highLight.png"
-    },
-    {
-		"id":"turnLight",
-      "imgUrl": "/image/light/turnLight.png"
-    },
-    {
-		"id":"switch",
-      "imgUrl": "/image/light/switch.png"
-    },
-    {
-		"id":"highLamp",
-      "imgUrl": "/image/light/highLamp.png"
-    },
-    {
-		"id":"lowLamp",
-      "imgUrl": "/image/light/lowLamp.png"
-    },
-    {
-		"id":"positionLamp",
-      "imgUrl": "/image/light/positionLamp.png"
+		"bgImg": "/image/light/bg.png",
+		"alarmImg": "/image/light/alarm.png",
+		"highLightImg": "/image/light/highLight.png",
+		"turnLightImg": "/image/light/turnLight.png",
+		"switchImg": "/image/light/switch.png",
+		"highLampImg": "/image/light/highLamp.png",
+		"lowLampImg": "/image/light/lowLamp.png",
+		"positionLampImg": "/image/light/positionLamp.png"
     }
   ]
 }
